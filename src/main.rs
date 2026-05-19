f7b4ddbc7cf90b9732ebbb7c0d80ad690c617521
Dmitry Shemin <shemindmitry@gmail.com>
2026-05-19 10:32:22 +0700

    chore: add logging

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@ mod handler;
 use std::time::Duration;
 
 use axum::{Router, http::StatusCode, routing::get};
-use tower_http::timeout::TimeoutLayer;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<(), impl std::error::Error> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
         .route("/", get(handler::index_handler))
         .route("/{*file}", get(handler::static_handler))
         .layer(TimeoutLayer::with_status_code(
             StatusCode::REQUEST_TIMEOUT,
             Duration::from_secs(10),
-        ));
+        ))
+        .layer(TraceLayer::new_for_http());
 
+    info!("wtimer started");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown())
@@ -38,5 +46,7 @@ async fn shutdown() {
     tokio::select! {
         _ = ctrl_c => {},
         _ = terminate => {},
-    }
+    };
+
+    info!("wtimer stopped");
 }
